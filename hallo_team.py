ef8adee1fa4b3791c6b54c311fd3e1cb5a2a97a3
mdurukan01 <mdurukan@tsn.at>
2026-05-21 16:51:27 +0200

    Update hallo_team.py
    1. Der Anfang (V.01)

--- hallo_team.py
+++ hallo_team.py
@@ -1,3 +1,16 @@
-print("Hallo Team!")
-print("MERTO")
-print("MEHMET") 
+#Streaming Datenbanken
+#Das Pythong Programm dient als einen Streaming-Dienst-System, in dem Nutzer ein ABo besitzen und Filme auf ihrer Wathchlist
+#21.05.2026
+
+
+import sqlite3
+import tkinter as tk
+from tkinter import messagebox
+
+#1. DATENBANK DIENST-FUNKTIONEN
+
+def hole_alle_nutzer():
+    # Verbindung öffnen, Daten holen, Verbindung schließen
+    connection = sqlite3.connect("streaming.db")
+    cursor = connection.cursor()
+    
